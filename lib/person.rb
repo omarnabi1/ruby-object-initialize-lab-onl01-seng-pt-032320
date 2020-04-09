24c86253ee3734f4daa22ca2f29961e99337c366
Omar Nabi <onabi20@gmail.com>
2020-04-09 03:17:16 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,9 +1,9 @@
 class Person
-  def initialize(name)
-    @name = name
+  def intialize (name)
+    @name = name 
   end
   def name=(name)
-    @name=name
+    @name = name
   end
   def name
     @name
